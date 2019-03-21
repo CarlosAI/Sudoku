74f49edefbc8e331fbf1e429a9822a2fd5b3dc07
CarlosAI <carloswesker@outlook.es>
2019-03-20 22:21:00 -0600

    done

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_20_084711) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: :cascade do |t|
     t.string "nombre"
     t.date "tiempo1"
